597725e963117a687a36a2540fd9addf913b1ad4
albeltra <alexbeltran@berkeley.edu>
2023-06-13 12:38:12 -0700

    debug

--- scripts/extract_subtitle.py
+++ scripts/extract_subtitle.py
@@ -8,6 +8,8 @@ mediaFile = json.loads(os.environ.get('mediaFile'))
 mediaInfo = json.loads(os.environ.get('mediaInfo'))
 stream_index = os.environ.get('stream_index', '0')
 
+print(mediaFile)
+
 extract_subtitle = Utils().extract_matroska_subtitle
 
 kwargs = {'mkv_file_path': mediaFile['path'],
