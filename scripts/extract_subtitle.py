987d50fd5818bf03e8807d7e8da2183d2ae8ee75
albeltra <alexbeltran@berkeley.edu>
2023-06-16 12:45:20 -0700

    more changes

--- scripts/extract_subtitle.py
+++ scripts/extract_subtitle.py
@@ -8,14 +8,15 @@ mediaFile = ast.literal_eval(os.environ.get('mediaFile'))
 mediaInfo = ast.literal_eval(os.environ.get('mediaInfo'))
 stream_index = os.environ.get('stream_index', '0')
 
-print(mediaFile) 
-
 extract_subtitle = Utils().extract_matroska_subtitle
+
+output_file_path = "/data/" + Path(mediaFile['path']).name + '.srt'
 
 kwargs = {'mkv_file_path': mediaFile['path'],
           'stream_index': stream_index,
-          'output_file_path': "/TEMP-SUBS/" + Path(mediaFile['path']).name + '.srt',
+          'output_file_path': output_file_path,
           'timeout_secs': 10000
           }
 
-extract_subtitle(**kwargs)
+if not os.path.exists(output_file_path):
+    extract_subtitle(**kwargs)
