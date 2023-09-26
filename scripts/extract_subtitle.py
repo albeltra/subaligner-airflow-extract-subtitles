29fb2e706b713a8736b5aac5ff6e8b585e8b9656
Alex Beltran <alexbeltran@berkeley.edu>
2023-09-26 02:53:46 -0700

    better stream index parsing

--- scripts/extract_subtitle.py
+++ scripts/extract_subtitle.py
@@ -6,14 +6,14 @@ from subaligner.utils import Utils
 
 mediaFile = ast.literal_eval(os.environ.get('mediaFile'))
 mediaInfo = ast.literal_eval(os.environ.get('mediaInfo'))
-stream_index = os.environ.get('stream_index', '0')
+stream_index = os.environ.get('stream_index')
 
 extract_subtitle = Utils().extract_matroska_subtitle
 
 output_file_path = "/audio-subs/" + Path(mediaFile['path']).name
 
 kwargs = {'mkv_file_path': mediaFile['path'], 
-          'stream_index': stream_index,
+          'stream_index': stream_index if stream_index else 0,
           'output_file_path': output_file_path + '.srt.srt',
           'timeout_secs': 10000
           }
